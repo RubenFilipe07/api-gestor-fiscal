b2fe1fc210fd31e57274086fb5d3f09242266b0f
Rúben Filipe <rubenfilipe.net@gmail.com>
2022-11-11 13:32:44 -0300

    implementando páginas sobre e contato

--- gestor-fiscal-front/src/pages/Contato.jsx
+++ gestor-fiscal-front/src/pages/Contato.jsx
@@ -1,12 +1,13 @@
-import React, { Fragment } from "react";
+import { React, Fragment } from "react";
 import 'antd/dist/antd.min.css';
 import Menu from "../components/Menu/Menu";
 import FooterContent from "../components/FooterContent/FooterContent";
-
-import { Layout } from "antd";
+import { Link } from "react-router-dom";
+import { Card, Layout, Breadcrumb } from "antd";
 
 const { Header, Content, Footer } = Layout;
-const Contato = () => (
+
+const contato = () => (
     <Fragment>
         <Layout>
             <Header>
@@ -14,14 +15,27 @@ const Contato = () => (
             </Header>
 
             <Content className="content">
-            
+
+                <Breadcrumb style={{ margin: '16px'}}>
+                    <Breadcrumb.Item><Link to="/">Início</Link></Breadcrumb.Item>
+                    <Breadcrumb.Item><Link to="/contato">Contato</Link></Breadcrumb.Item>
+                </Breadcrumb>
+
+                <Card title="Contato do desenvolvedor" bordered={false} style={{ width: 400, margin: '200px auto' }}>
+                    <p>Telefone: (84) 981593183</p>
+                    <p>Email: <a href="https://mailto:rubenfilipe.net@gmail.com">rubenfilipe.net@gmail.com</a></p>
+                    <p>GitHub: <a href="https://github.com/rubenfilipe07">github.com/rubenfilipe07</a></p>
+                    <p>Telegram: <a href="https://t.me/ruben_filipe07">t.me/ruben_filipe07</a></p>
+                </Card>
+
+
             </Content>
-            
+
             <Footer className="footer">
-                <FooterContent/>
+                <FooterContent />
             </Footer>
         </Layout>
     </Fragment>
 );
 
-export default Contato;
+export default contato;
--- gestor-fiscal-front/src/pages/Sobre.jsx
+++ gestor-fiscal-front/src/pages/Sobre.jsx
@@ -2,8 +2,10 @@ import React, { Fragment } from "react";
 import 'antd/dist/antd.min.css';
 import Menu from "../components/Menu/Menu";
 import FooterContent from "../components/FooterContent/FooterContent";
+import { Link } from "react-router-dom";
+import '../components/HomeContent/HomeContent.css';
 
-import { Layout } from "antd";
+import { Layout, Breadcrumb, Card, Button, Col, Row, Divider } from "antd";
 
 const { Header, Content, Footer } = Layout;
 const Sobre = () => (
@@ -14,11 +16,39 @@ const Sobre = () => (
             </Header>
 
             <Content className="content">
-            
+
+                <Breadcrumb style={{ margin: '16px' }}>
+                    <Breadcrumb.Item><Link to="/">Início</Link></Breadcrumb.Item>
+                    <Breadcrumb.Item><Link to="/sobre">Sobre</Link></Breadcrumb.Item>
+                </Breadcrumb>
+
+                <Row gutter={10} className="row-cards">
+                    <Col span={5}>
+
+                        <Card style={{ width: '300px' }} bordered={false}>
+                            <h1>Donwload do Projeto</h1>
+                            <p>Este é um projeto open source e está listado no repositório abaixo</p>
+                            <Divider />
+                            <Button type="primary" style={{ width: '100%' }}>
+                                <a className="link" href="https://github.com/RubenFilipe07/spring-react-gestor-fiscal/archive/refs/heads/main.zip">Donwload</a>
+                            </Button>
+                        </Card>
+                    </Col>
+                    <Col span={5}>
+
+                        <Card style={{ width: '300px' }} bordered={false}>
+                            <h1>Documentação do Projeto</h1>
+                            <p>A documentação descreve como o projeto funciona e como ele foi feito</p>
+                            <Divider />
+                            <Button type="primary" style={{ width: '100%' }}><a href="https://github.com/RubenFilipe07/spring-react-gestor-fiscal/blob/main/README.md">Acessar</a></Button>
+                        </Card>
+                    </Col>
+                </Row>
+
             </Content>
-            
+
             <Footer className="footer">
-                <FooterContent/>
+                <FooterContent />
             </Footer>
         </Layout>
     </Fragment>
