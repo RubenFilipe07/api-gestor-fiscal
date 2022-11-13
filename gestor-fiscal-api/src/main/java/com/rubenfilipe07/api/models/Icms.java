89b47e28a02a95543dd65549de4ba1b410bdd081
Rúben Filipe <rubenfilipe.net@gmail.com>
2022-11-13 03:40:47 -0300

    alterando strategy de AUTO para IDENTITY no id

--- gestor-fiscal-api/src/main/java/com/rubenfilipe07/api/models/Icms.java
+++ gestor-fiscal-api/src/main/java/com/rubenfilipe07/api/models/Icms.java
@@ -15,7 +15,7 @@ public class Icms implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
 	private String estado;
