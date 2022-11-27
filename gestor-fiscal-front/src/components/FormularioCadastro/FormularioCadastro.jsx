36e260904df7679c1cc6421e76f8e925ab6fa5c2
Rúben Filipe <rubenfilipe.net@gmail.com>
2022-11-27 02:20:19 -0300

    alterando name dos inputs no FormularioCadastro

--- gestor-fiscal-front/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ gestor-fiscal-front/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,9 +6,15 @@ import './FormularioCadastro.css'
   
 const formulario = (props) => (
      <Form className="form-cadastrar-item">
-              <Form.Item name="nome-item" onChange={props.handleChangeName} label={props.campoNomeFormulario} required rules={[{ required: true }]}>
+             {props.campoNomeFormulario === "Nome do Estado:" ? (
+                 <Form.Item name="nome-estado" onChange={props.handleChangeName} label={props.campoNomeFormulario} required rules={[{ required: true }]}>
+                 <Input />
+               </Form.Item>
+              ) : (
+                <Form.Item name="nome-produto" onChange={props.handleChangeName} label={props.campoNomeFormulario} required rules={[{ required: true }]}>
                 <Input />
-              </Form.Item>
+              </Form.Item>)
+              }
               {props.campoNomeFormulario === "Nome do Estado:" ? (
                 <Form.Item name="sigla-estado" onChange={props.handleChangeSigla} label="Sigla" required rules={[{ required: true }]}>
                   <Input />
